592e81b4a06fa4d2f5c1da3d49bd7d1eeec2f9ca
Dennis von Bargen <dennis@vonbargen.se>
2021-11-25 14:10:34 +0100

    Reverted to remote exec for a more stable SSH connection

--- vpn-server/main.tf
+++ vpn-server/main.tf
@@ -14,12 +14,25 @@ resource "linode_instance" "vpn-server" {
     type            = var.type
     authorized_keys = values(var.public_keys)
 
+    provisioner "remote-exec" {
+        inline = [
+            "echo SSH connection stable"
+        ]
+
+        connection {
+            host        = self.ip_address
+            type        = "ssh"
+            user        = "root"
+            private_key = file(var.pvt_key)
+        }
+    }
+
     provisioner "local-exec" {
         command = <<-EOT
             mkdir ~/tmp
             mkdir ~/tmp/${var.name}
             curl https://raw.githubusercontent.com/Dekamik/vpn-modules/main/vpn-server/ovpn-install.yml > ~/tmp/${var.name}/ovpn-install.yml
-            ansible-playbook -vvv -u root -i '${self.ip_address},' --private-key ${var.pvt_key} -e 'server_name=${var.name} dl_dir=${var.download_dir}' ~/tmp/${var.name}/ovpn-install.yml
+            ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ip_address},' --private-key ${var.pvt_key} -e 'server_name=${var.name} dl_dir=${var.download_dir}' ~/tmp/${var.name}/ovpn-install.yml
             rm -rf ~/tmp/${var.name}
             find ~/tmp -maxdepth 0 -empty -exec rmdir ~/tmp \;
         EOT
